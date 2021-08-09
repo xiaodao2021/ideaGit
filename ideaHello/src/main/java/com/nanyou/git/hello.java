85aef4bfb692309856873233f494ab3055c4020c
xiaodao2021 <813259374@qq.com>
2021-08-09 16:12:00 +0800

    主干添加

--- ideaHello/src/main/java/com/nanyou/git/hello.java
+++ ideaHello/src/main/java/com/nanyou/git/hello.java
@@ -6,5 +6,6 @@ public class hello {
         System.out.println("idea hello--2");
         System.out.println("idea hello--3");
         System.out.println("创建分支");
-    }
+        System.out.println("主干添加");
+}
 }
