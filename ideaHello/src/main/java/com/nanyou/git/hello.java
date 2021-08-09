0eb907f7c48ae8562b0af4059e5b68fc2fdd3758
xiaodao2021 <813259374@qq.com>
2021-08-09 15:53:47 +0800

    第一次添加

---
+++ ideaHello/src/main/java/com/nanyou/git/hello.java
@@ -0,0 +1,10 @@
+package com.nanyou.git;
+/**
+ * @author shkstart
+ * @create 2021-08-09 15:42
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("idea hello");
+    }
+}
