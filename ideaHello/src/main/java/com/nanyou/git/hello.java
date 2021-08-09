b70f429e4836dd827b3803f149b891de57c3e8a2
xiaodao2021 <813259374@qq.com>
2021-08-09 15:55:11 +0800

    第一次修改

--- ideaHello/src/main/java/com/nanyou/git/hello.java
+++ ideaHello/src/main/java/com/nanyou/git/hello.java
@@ -6,5 +6,7 @@ package com.nanyou.git;
 public class hello {
     public static void main(String[] args) {
         System.out.println("idea hello");
+        System.out.println("idea hello--1");
+
     }
 }
